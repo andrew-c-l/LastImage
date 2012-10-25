15e1e5d495c8802fe89d04c5e0100f4b8b950e6c
Andrew Lim <andrew.c.lim@gmail.com>
2012-10-25 10:49:00 +0100

    Added more examples

--- index.php
+++ index.php
@@ -4,17 +4,41 @@ $lastfm = LastImage::getInstance();
 $lastfm->setApiKey("your last.fm api key");
 $lastfm->setCacheDir("cache/");
 $lastfm->setDefaultImage("cache/default.jpeg");
-$i = $lastfm->grab("Cher", "", "", "medium");
+$artist_image = $lastfm->grab("Cher", "", "", "medium");
+$track_image = $lastfm->grab("Cher", "Believe", "", "medium");
+$album_image = $lastfm->grab("Cher", "", "Believe", "medium");
 ?>
 
 <h1>LastImage</h1>
 
 LastImage - PHP Wrapper for the Last.fm API for images. 
 
+<h2>Artist Image</h2>
+
 <ul>
 
-<li><?php echo $i; ?></li>
+<li><?php echo $artist_image; ?></li>
 
-<li><img src="<?php echo $i; ?>"></li>
+<li><img src="<?php echo $artist_image; ?>"></li>
+
+</ul>
+
+<h2>Track Image</h2>
+
+<ul>
+
+<li><?php echo $track_image; ?></li>
+
+<li><img src="<?php echo $track_image; ?>"></li>
+
+</ul>
+
+<h2>Album Image</h2>
+
+<ul>
+
+<li><?php echo $album_image; ?></li>
+
+<li><img src="<?php echo $album_image; ?>"></li>
 
 </ul>
